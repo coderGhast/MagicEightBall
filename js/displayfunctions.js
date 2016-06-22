2833b6e61d7ca8dc9fb018aa6f43317669b4daad
James <euesdenje@gmail.com>
2016-06-21 23:42:24 -0600

    New fonts, some design niceties
    Added a nicer font and made the questions into a list. I can do a couple
    more shiny things and then stop.

--- js/displayfunctions.js
+++ js/displayfunctions.js
@@ -1,10 +1,11 @@
 // Putting the content of the previous questions list into the correct element
 function display_questions_list(){
     var questions_list_div = document.getElementById("questions_list");
-    var questions_list_text = "";
+    var questions_list_text = "<ul id=\"questions_list_element\">";
     for(var i = user_data.previous_questions.length -1; i >= 0; i--){
-        questions_list_text += user_data.previous_questions[i] + "<br />";
+        questions_list_text +="<li>" + user_data.previous_questions[i] + "</li>";
     }
+    questions_list_text += "</li></ul>"
     questions_list_div.innerHTML = questions_list_text;
 }
 
