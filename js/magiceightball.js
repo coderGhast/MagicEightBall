90229bb156e4f20f56e05fc71213e46329f92d9f
James <euesdenje@gmail.com>
2016-06-21 19:58:59 -0600

    Getting response text
    Checking that the response text we get matches what we expect to receive
    from the provided response number.

--- js/magiceightball.js
+++ js/magiceightball.js
@@ -20,3 +20,72 @@ function get_new_response_number(last_response) {
 
     return current_response;
 }
+
+function get_response_text(response_number){
+    var response;
+    switch(response_number){
+        case(1) : 
+            response = "It is certain"
+            break; 
+        case(2) :
+            response = "It is decidedly so"
+            break;
+        case(3) :
+            response = "Without a doubt"
+            break;
+        case(4) :
+            response = "Yes, definitely"
+            break;
+        case(5) :
+            response = "You may rely on it"
+            break;
+        case(6) :
+            response = "As I see it, yes"
+            break;
+        case(7) :
+            response = "Most likely"
+            break;
+        case(8) :
+            response = "Outlook good"
+            break;
+        case(9) :
+            response = "Yes"
+            break;
+        case(10) :
+            response = "Signs point to yes"
+            break;
+        case(11) :
+            response = "Reply hazy try again"
+            break;
+        case(12) :
+            response = "Ask again later"
+            break;
+        case(13) :
+            response = "Better not tell you now"
+            break;
+        case(14) :
+            response = "Cannot predict now"
+            break;
+        case(15) :
+            response = "Concentrate and ask again"
+            break;
+        case(16) :
+            response = "Don't count on it"
+            break;
+        case(17) :
+            response = "My reply is no"
+            break;
+        case(18) :
+            response = "My sources say no"
+            break;
+        case(19) :
+            response = "Outlook not so good"
+            break;
+        case(20) :
+            response = "Very doubtful"
+            break;
+        default :
+            response = "Failed to understand question"
+    }
+    return response;
+}
--- js/tests.js
+++ js/tests.js
@@ -15,4 +15,28 @@ QUnit.test("should not get the same response twice in a row", function( assert )
         var response_number = get_new_response_number(last_response);
         assert.ok(response_number != last_response, "Passed! Number was not the same. Received:" + response_number + " Number to avoid:" + last_response);
     }
-})
+});
+
+// When we ask for a response text, we expect to see the (known) response corresponding to that code.
+QUnit.test("should return the expected response for the response number", function( assert ) {
+    assert.equal(get_response_text(1), "It is certain");
+    assert.equal(get_response_text(2), "It is decidedly so");
+    assert.equal(get_response_text(3), "Without a doubt");
+    assert.equal(get_response_text(4), "Yes, definitely");
+    assert.equal(get_response_text(5), "You may rely on it");
+    assert.equal(get_response_text(6), "As I see it, yes");
+    assert.equal(get_response_text(7), "Most likely");
+    assert.equal(get_response_text(8), "Outlook good");
+    assert.equal(get_response_text(9), "Yes");
+    assert.equal(get_response_text(10), "Signs point to yes");
+    assert.equal(get_response_text(11), "Reply hazy try again");
+    assert.equal(get_response_text(12), "Ask again later");
+    assert.equal(get_response_text(13), "Better not tell you now");
+    assert.equal(get_response_text(14), "Cannot predict now");
+    assert.equal(get_response_text(15), "Concentrate and ask again");
+    assert.equal(get_response_text(16), "Don't count on it");
+    assert.equal(get_response_text(17), "My reply is no");
+    assert.equal(get_response_text(18), "My sources say no");
+    assert.equal(get_response_text(19), "Outlook not so good");
+    assert.equal(get_response_text(20), "Very doubtful");   
+});
